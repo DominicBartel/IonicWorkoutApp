117afde77070aa0b746fa5aabfafe72c901b5e48
unknown <DominicBartel@gmail.com>
2018-05-27 02:35:22 -0400

    added functional timer to timer page

--- src/pages/timer/timer.ts
+++ src/pages/timer/timer.ts
@@ -12,17 +12,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class TimerPage {
   timerIcon = 'play';
   params;
-  workout;
-  timeLeft;
+  workout = "Get Ready!";
+  timeLeft = 3;
   paused = false;
+  currentWorkout = -1;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.params = navParams.data;
   }
 
   ionViewDidLoad() {
-    
-    
+    this.timer();
+
   }
+  
+  timer() {
+    setInterval(x => {
+
+      if (!this.paused) {
+
+        if (this.timeLeft > 1) {
+          this.timeLeft--;
+        }
+        else if (this.currentWorkout < this.params.reps.length - 1) {
+          this.currentWorkout++;
+          console.log(this.currentWorkout)
+          this.workout = this.params.reps[this.currentWorkout].workout;
+          this.timeLeft = this.params.reps[this.currentWorkout].time;
+        }else{
+          this.workout = "Done!"
+          this.timeLeft = 0;
+        }
+
+
+      }
+    }, 1000);
+
+
+  }
+
 
 }
